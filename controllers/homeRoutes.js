76a5039ec22a604eae57d5bb0ec4b94030baa7db
Ian Darland <iansmbpro@ians-mbp.lan>
2021-05-04 11:36:39 -0500

    saving changees updated poke fetch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Pokemon } = require('../models');
+const withAuth = require("../util/auth.js")
 
 router.get('/', async (req, res) => {
     try {
@@ -23,6 +24,25 @@ router.get('/', async (req, res) => {
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const pokeData = await fetch("https://pokeapi.co/api/v2/pokemon?offset=300&limit=100")
+        const pokemon = await pokeData.json();
+        
+        const pokes = pokemon.map(item => {
+            return { name: item.forms[0].name, image: item.sprites.front_default }
+        })
+
+        if(req.session.logged_in) {
+            res.render('dashboard',
+            {pokes});
+            return
+        }
+    } catch (err) {
+        res.status(500)
     }
 });
 
